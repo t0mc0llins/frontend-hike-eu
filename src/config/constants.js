5205e166ed94d5e8127f196843cc18bf094f6de9
t0mc0llins <tomcollins93@gmail.com>
2022-05-12 17:12:03 +0200

    api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
-// export const apiUrl = "https://hike-eu.herokuapp.com";
-export const apiUrl = "http://localhost:4000";
+export const apiUrl =
+  "https://hike-eu.herokuapp.com" || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const noImage =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
